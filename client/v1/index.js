08081f3deb5b5616fc1db2b7315c0e24514bdfb5
clarissesacre <clarisse.sacre@outlook.com>
2023-02-07 19:40:03 +0100

    Update index.js

--- client/v1/index.js
+++ client/v1/index.js
@@ -493,7 +493,7 @@ console.log("todo 2");
 var reasonable_price=100;
 var reasonable_store = "True";
 console.log(COTELE_PARIS[0]['price']);
-for(let i = 0 ; i<=COTELE_PARIS.length ; i++)
+for(let i = 0 ; i<COTELE_PARIS.length ; i++)
 {
   if(COTELE_PARIS[i]['price']>=100)
   {
