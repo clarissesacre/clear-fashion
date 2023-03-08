b9a910336e32585d90f79bb15cc304ba5fb19244
clarissesacre <clarisse.sacre@outlook.com>
2023-03-08 14:08:05 +0100

    Update sandbox.js

--- server/sandbox.js
+++ server/sandbox.js
@@ -72,9 +72,27 @@ async function sandbox_circle (eshop = 'https://shop.circlesportswear.com/collec
   }
 }
 
+
+async function sandbox_circle_women (eshop = 'https://shop.circlesportswear.com/collections/collection-femme') {
+  try {
+    console.log(`🕵️‍♀️  browsing ${eshop} eshop`);
+
+    const products = await circlebrand.scrape(eshop); // lance dedicatedbrand.js puis affiche les produits
+
+    console.log(products);
+    console.log('done');
+    process.exit(0);
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+}
+
+
 const [,, eshop] = process.argv;
 
 //sandbox_dedicated(eshop);
 //sandbox_montlimart(eshop);
 //sandbox_circle(eshop);
 //sandbox_dedicated_women(eshop);
+sandbox_circle_women(eshop);
