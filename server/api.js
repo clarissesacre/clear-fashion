6029198d48aedc81ca53e71a43b82c8e5667177f
clarissesacre <clarisse.sacre@outlook.com>
2023-04-01 18:39:10 +0200

    Update api.js

--- server/api.js
+++ server/api.js
@@ -30,74 +30,45 @@ app.get('/', (request, response) => {
   response.send({'ack': false});
 });
 
-// affiche les categories des produits chez circle
+
+// affiche les marques dispo
 app.get('/brands', async (request, response) => {
-  try{
-    const client = getClient();
-    const collection = client.db("ClusterClearFashion").collection("all_brands");
-    const found = await collection.distinct('shopname');
-    //response.send({brands: found});
-    response.json(found);
-  }
-  catch{
-    response.send({error : "Couldn't fetch brands"}); 
-  }
+  const client = getClient();
+  const collection = client.db("ClusterClearFashion").collection("all_brands");
+  const found = await collection.distinct('shopname');
+  response.json(found);
 });
 
-
+// affiche tous les produits dispo
 app.get('/products', async (request, response) => {
   const client = getClient();
-  const collection = client.db("ClusterClearFashion").collection("GENERAL");
-
+  const collection = client.db("ClusterClearFashion").collection("all_brands");
   const result = await collection.find({}).toArray();
-
   response.json(result);
 });
 
-app.get('/products/search', async (request, response) => {
+// selectionne des articles selon prix ou marque
+app.get('/products/price', async (request, response) => {
   try{
     const client = getClient();
-    const collection = client.db("ClusterClearFashion").collection("GENERAL");
-
+    const collection = client.db("ClusterClearFashion").collection("all_brands");
     var script ={};
-    var page = request.query.page;
-    var limit = request.query.limit;
     var price = request.query.price;
-    var brand = request.query.brand;
+    var shopname = request.query.shopname;
 
-    if(page == undefined){
-      page = 1;
-    }
-    else{
-      page = parseInt(page);
-    }
-
-    if(limit == undefined){
-      limit = 12;
-    }
-    else{
-      limit = parseInt(limit);
-    }
-    const skip = (page - 1) * limit;
-
-    if((brand!="")){
-      script.brand = brand;
-    }
-
+    // affiche tout si aucun prix max rentré
     if(price!=""){
       script.price = {$lte: parseFloat(price)};
     }
-
-    const count = await collection.countDocuments(script);
-    const totalPages = Math.ceil(count / limit);
-
-    const result = await collection.find(script).skip(skip).limit(limit).toArray();
+    // affiche toutes les marques si aucune rentrée
+    if((shopname!="")){
+      script.shopname = shopname;
+    }
+      
+    const result = await collection.find(script).toArray();
 
     response.json({
-      currentPage: page,
-      totalPages: totalPages,
-      totalCount: count,
-      data: result
+      result
     });
   }
   catch{
@@ -105,48 +76,7 @@ app.get('/products/search', async (request, response) => {
   }
 });
 
-app.get('/products/id', async (request, response) => {
-  try{
-    const productId = request.params.id;
-    const script = {_id: ObjectId(productId)};
-    const client = getClient();
-    const collection = client.db("ClusterClearFashion").collection("GENERAL");
-    const found = await collection.find(script).toArray();
-    
-    response.send({ids: found});
-
-  } catch(err) {
-	  response.send({error : "ID not found"});  
-  }
-});
 
 app.listen(PORT);
 
 console.log(`📡 Running on port ${PORT}`);
-
-
-
-/*const cors = require('cors');
-const express = require('express');
-const helmet = require('helmet');
-
-const PORT = 8092;
-
-const app = express();
-
-module.exports = app;
-
-app.use(require('body-parser').json());
-app.use(cors());
-app.use(helmet());
-
-app.options('*', cors());
-
-app.get('/', (request, response) => {
-  response.send({'ack': true});
-});
-
-app.listen(PORT);
-
-console.log(`📡 Running on port ${PORT}`);
-*/
