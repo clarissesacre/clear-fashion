036096a04f9ac63ab1e5fe8ef49d320154b0ec6e
clarissesacre <clarisse.sacre@outlook.com>
2023-04-01 17:56:16 +0200

    Update api.js

--- server/api.js
+++ server/api.js
@@ -4,6 +4,133 @@ const helmet = require('helmet');
 
 const PORT = 8092;
 
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const fs = require('fs');
+const { Console } = require('console');
+
+function getClient() {
+  const uri = "mongodb+srv://clarissesacre:clarisse@clusterclearfashion.7kttiwq.mongodb.net/test?retryWrites=true&w=majority";
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+  return client;
+}
+
+const app = express();
+
+module.exports = app;
+
+app.use(require('body-parser').json());
+app.use(cors());
+app.use(helmet());
+
+app.options('*', cors());
+
+// GET http://localhost:8092 sur Insomnia
+// affiche {'ack': false}
+app.get('/', (request, response) => {
+  response.send({'ack': false});
+});
+
+
+app.get('/category_circle', async (request, response) => {
+  try{
+    const client = getClient();
+    const collection = client.db("ClusterClearFashion").collection("circle");
+    const found = await collection.distinct('category');
+    //response.send({brands: found});
+    response.json(found);
+  }
+  catch{
+    response.send({error : "Couldn't fetch brands"}); 
+  }
+});
+
+app.get('/products', async (request, response) => {
+  const client = getClient();
+  const collection = client.db("ClusterClearFashion").collection("GENERAL");
+
+  const result = await collection.find({}).toArray();
+
+  response.json(result);
+});
+
+app.get('/products/search', async (request, response) => {
+  try{
+    const client = getClient();
+    const collection = client.db("ClusterClearFashion").collection("GENERAL");
+
+    var script ={};
+    var page = request.query.page;
+    var limit = request.query.limit;
+    var price = request.query.price;
+    var brand = request.query.brand;
+
+    if(page == undefined){
+      page = 1;
+    }
+    else{
+      page = parseInt(page);
+    }
+
+    if(limit == undefined){
+      limit = 12;
+    }
+    else{
+      limit = parseInt(limit);
+    }
+    const skip = (page - 1) * limit;
+
+    if((brand!="")){
+      script.brand = brand;
+    }
+
+    if(price!=""){
+      script.price = {$lte: parseFloat(price)};
+    }
+
+    const count = await collection.countDocuments(script);
+    const totalPages = Math.ceil(count / limit);
+
+    const result = await collection.find(script).skip(skip).limit(limit).toArray();
+
+    response.json({
+      currentPage: page,
+      totalPages: totalPages,
+      totalCount: count,
+      data: result
+    });
+  }
+  catch{
+    response.send({error : "Couldn't fetch searchs"}); 
+  }
+});
+
+app.get('/products/id', async (request, response) => {
+  try{
+    const productId = request.params.id;
+    const script = {_id: ObjectId(productId)};
+    const client = getClient();
+    const collection = client.db("ClusterClearFashion").collection("GENERAL");
+    const found = await collection.find(script).toArray();
+    
+    response.send({ids: found});
+
+  } catch(err) {
+	  response.send({error : "ID not found"});  
+  }
+});
+
+app.listen(PORT);
+
+console.log(`📡 Running on port ${PORT}`);
+
+
+
+/*const cors = require('cors');
+const express = require('express');
+const helmet = require('helmet');
+
+const PORT = 8092;
+
 const app = express();
 
 module.exports = app;
@@ -21,3 +148,4 @@ app.get('/', (request, response) => {
 app.listen(PORT);
 
 console.log(`📡 Running on port ${PORT}`);
+*/
