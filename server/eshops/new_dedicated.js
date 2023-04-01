ad6df1df00a0b42ba8be5660a6f06fc4f23b5548
clarissesacre <clarisse.sacre@outlook.com>
2023-04-01 11:29:58 +0200

    Create new_dedicated.js

---
+++ server/eshops/new_dedicated.js
@@ -0,0 +1,64 @@
+// fonction pour scrapper
+
+// pour DEDICATED brand 
+// faire un truc comme ça par site
+// créer un autre .js pour les autres sites 
+
+// on envoie le link initial : eshop = https://www.dedicatedbrand.com/en/
+// const products = await new_dedicated.scrape(eshop); 
+      
+
+const fetch = require('node-fetch');
+const cheerio = require('cheerio');
+
+/**
+ * Parse webpage e-shop
+ * @param  {String} data - html response
+ * @return {Array} products
+ */
+const parse = data => {
+  const $ = cheerio.load(data);
+
+  // d'abord on prend toutes les sous catégories du site : tshirt, pantalon etc.
+  console.log("Here is all the categories of circle's products");
+  return $('.mainNavigation-fixedContainer .mainNavigation-link-subMenu-link*')
+    .map((i, element) => {
+      var category = $(element)
+      .find('a')
+      .prop("href");
+      console.log(category);
+      category = category.slice(4);
+      console.log('after slice');
+      console.log(category);
+      return [category];
+    })
+    .get();
+};
+
+
+
+
+// pas touche
+/**
+ * Scrape all the products for a given url page
+ * @param  {[type]}  url
+ * @return {Array|null}
+ */
+module.exports.scrape = async url => {
+    try {
+      const response = await fetch(url);
+  
+      if (response.ok) {
+        const body = await response.text();
+  
+        return parse(body);
+      }
+  
+      console.error(response);
+  
+      return null;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  };
