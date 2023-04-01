83fe0ee5e50eabf63461d063a34ba7162033356b
clarissesacre <clarisse.sacre@outlook.com>
2023-04-01 13:01:18 +0200

    Update dedicated.js

--- server/eshops/dedicated.js
+++ server/eshops/dedicated.js
@@ -14,7 +14,7 @@ const cheerio = require('cheerio');
  */
 const parse = data => {
   const $ = cheerio.load(data);
-
+  
   return $('.productList-container .productList') // on va analyser tous les éléments 1 par 1
   // productList-container : endroit où y'a les produits
   // productList : où y'a un produit
@@ -35,11 +35,42 @@ const parse = data => {
           .text()
       );
 
+
+
       
-        const shopname = 'dedicated';
+      const shopname = 'dedicated';
 
+      var link = $(element)
+      .find('.productList-link')
+      .attr('href');
+      final_link = 'https://www.dedicatedbrand.com' + link;
+      
+      var for_who= '';
+      var category='';
+      if(link.search('/en/men/')==0)
+      {
+        for_who = 'men';
+        category = link.replace('/en/men/','');
+        category = category.split('/')[0];
+      }
+      if(link.search('/en/women/')==0)
+      {
+        for_who = 'women';
+        category = link.replace('/en/women/','');
+        category = category.split('/')[0];
+      }
+      if(link.search('/en/kids/')==0)
+      {
+        for_who = 'kids';
+        category = link.replace('/en/kids/','');
+        category = category.split('/')[0];
+      }
+      
 
-      return {shopname, name, color, price};
+      
+      
+
+      return {shopname, name, color, price, for_who, category, final_link};
     })
     .get();
 };
