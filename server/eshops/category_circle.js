98d1028ddf00140b42eb153cefd2e773e6616b7c
clarissesacre <clarisse.sacre@outlook.com>
2023-04-01 14:12:12 +0200

    Update category_circle.js

--- server/eshops/category_circle.js
+++ server/eshops/category_circle.js
@@ -1,12 +1,6 @@
-// fonction pour scrapper les CATEGORIES 
-// pour circle brand 
-
-// faire un truc comme ça par site
-// créer un autre .js pour les autres sites 
-
-
 const fetch = require('node-fetch');
 const cheerio = require('cheerio');
+
 
 /**
  * Parse webpage e-shop
@@ -15,41 +9,37 @@ const cheerio = require('cheerio');
  */
 const parse = data => {
   const $ = cheerio.load(data);
-  // on prend toutes les sous catégories du site : tshirt, pantalon etc.
-  return $('.list-menu* .header__menu-item*')
-  .map((i, element) => {
-    var category = $(element)
-    .find('a')
-    .prop("href");
-    return [category];
-  })
-  .get();
+  return $('.wrapper-link-menu* .wrapper-*')
+    .map((i, element) => {
+      var category = $(element)
+      .find('a')
+      .prop("href");
+      console.log(category);
+      return [category];
+    })
+    .get();
 };
 
-
-
-
-// pas touche
 /**
  * Scrape all the products for a given url page
  * @param  {[type]}  url
  * @return {Array|null}
  */
 module.exports.scrape = async url => {
-    try {
-      const response = await fetch(url);
-  
-      if (response.ok) {
-        const body = await response.text();
-  
-        return parse(body);
-      }
-  
-      console.error(response);
-  
-      return null;
-    } catch (error) {
-      console.error(error);
-      return null;
+  try {
+    const response = await fetch(url);
+
+    if (response.ok) {
+      const body = await response.text();
+
+      return parse(body);
     }
-  };
+
+    console.error(response);
+
+    return null;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
