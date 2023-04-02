631052141fa73088c97f13db280616d318df8467
clarissesacre <clarisse.sacre@outlook.com>
2023-04-02 17:05:01 +0200

    Update portfolio.js

--- client/v3/portfolio.js
+++ client/v3/portfolio.js
@@ -119,6 +119,9 @@ const renderSearchProducts = products => {
         <span>${product.shopname}</span>
         <a href="${product.link}" target="_blank">${product.name}</a>
         <span>${product.price}€</span>
+        <img  src=${product.image} alt="Not avaible" width="200" height="220">
+
+
         <span>${product.image}</span>
         <button onclick="changeFavorite('${product._id}')">${textFavorite(product._id)}</button>
       </div>
