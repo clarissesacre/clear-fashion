bce70be4b875e45e669b4ec1771e1dc3ab0a26b4
clarissesacre <clarisse.sacre@outlook.com>
2023-04-02 11:50:48 +0200

    Update portfolio.js

--- client/v2/portfolio.js
+++ client/v2/portfolio.js
@@ -55,7 +55,7 @@ const fetchProducts = async (page = 1, size = 12) => {
     // page = 1 et size = 12
     const response = await fetch(
       //`https://clear-fashion-api.vercel.app?page=${page}&size=${size}`
-      'https://clear-fashion-beige.vercel.app/'
+      'http://localhost:8092/products'
     );
 
     // lit la réponse en tant que JSON
